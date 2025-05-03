082abe8e70287b081a35b79b3bde26381653ce11
BigPhil2010 <167675922+BigPhil2010@users.noreply.github.com>
2025-05-03 16:57:29 +0200

    script

--- scripts/search.gd
+++ scripts/search.gd
@@ -3,3 +3,13 @@ extends Node2D
 
 func _on_button_home_pressed():
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+
+set_buttons(5)
+
+func set_buttons(btn_count):
+	if btn_count == 1:
+		get_node("VBoxContainer/HBoxContainer/Button1").visible = true
+	if btn_count == 2:
+		get_node("VBoxContainer/HBoxContainer/Button1").visible = true
+		get_node("VBoxContainer/HBoxContainer/Button2").visible = true
